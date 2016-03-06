141fb68e6c3f43507214b69a03373f8b371ec053
Amir Mehdi Savand <amirm3hdi@gmail.com>
2016-03-06 22:41:36 +0400

    Admin setup, other

--- stickers/admin.py
+++ stickers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Sticker
+
+
+class StickerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'color')
+    search_fields = ['title', 'color']
+    list_filter = ['date', 'color']
+
+
+
+admin.site.register(User)
+admin.site.register(Sticker, StickerAdmin)
---
+++ stickers/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from stickers.decorators import redirect_if_logged_in
+
+from stickers import views
+
+
+app_name = 'stickers'
+urlpatterns = [
+    url(r'^$',          views.IndexView.as_view(), name='index'),
+    url(r'^login/$',    views.login_view,          name='login'),
+    url(r'^logout/$',   views.logout_view,         name='logout'),
+    url(r'^register/$', views.user_create,         name='register'),
+    url(r'^create/$',   views.sticker_create,      name='create'),
+    url(r'^delete/$',   views.sticker_delete,      name='delete'),
+]
