788ce677975eb42dcb76ea3689e4c0748b136943
Amir Mehdi Savand <amirm3hdi@gmail.com>
2016-03-08 20:57:07 +0400

    Better sticker handling

--- stickers/static/stickers/js/app.js
+++ stickers/static/stickers/js/app.js
@@ -6,10 +6,79 @@ sticker = {};
 
 function StickerInit() {
 
+    // Check for stickers
+    StickerNothing();
+
     // jQuery draggable init
+    StickerDraggable();
+
+    // jQuery speed scroll init
+    jQuery.scrollSpeed(500, 500);
+
+    // If there's any stickers
+    if (StickerCount() > 0) {
+
+        // If local storage is supported
+        if (typeof Storage !== "undefined") {
+
+            // If sticker is set in local storage
+            if (localStorage.sticker) {
+
+                sticker = JSON.parse(localStorage.sticker);
+
+                $.each(sticker, function(index, value) {
+
+                    if (parseInt(index) > 0) {
+
+                        var el = $('.sticker[data-id=' + index + ']');
+
+                        $(el).css('top',  value.top);
+                        $(el).css('left', value.left);
+                    }
+                });
+            }
+
+        // If not supported
+        } else alert('Oh... It seems that your browser does not support local storage.\nSticker may not work properly!');
+
+        // If there's not sticker saved in local storage
+        if (!localStorage.sticker) {
+
+            $('.sticker').each(function(index, value) {
+
+                StickerPosition(value);
+            });
+        }
+    }
+
+    // Stickers vissiblity init
+    $('.sticker').animate({ 'opacity': 1.0 });
+}
+
+function StickerCount() {
+    
+    return $('.sticker-container').children().length;
+}
+
+function StickerNothing() {
+    
+    if (StickerCount() < 1) {
+
+        $('.oh-no-stickers').fadeIn();
+        $('.sticker-container').hide();
+    
+    } else {
+
+        $('.sticker-container').fadeIn();
+        $('.oh-no-stickers').hide();
+    }
+}
+
+function StickerDraggable() {
+
     $(".sticker").draggable({
         handle: ".panel-heading",
-        containment: "section.page",
+        containment: ".sticker-container",
         scrollSensitivity: 100,
         // snap: ".sticker-outer",
         // snapMode: "outer",
@@ -26,48 +95,9 @@ function StickerInit() {
             $('audio:first-of-type')[0].play();
         }
     });
-
-    // jQuery speed scroll init
-    jQuery.scrollSpeed(500, 500);
-
-    // If local storage is supported
-    if (typeof Storage !== "undefined") {
-
-        // If sticker is set in local storage
-        if (localStorage.sticker) {
-
-            sticker = JSON.parse(localStorage.sticker);
-
-            $.each(sticker, function(index, value) {
-
-                var el = $('.sticker[data-id=' + index + ']');
-
-                $(el).css('top',  value.top );
-                $(el).css('left', value.left);
-            });
-        }
-
-    // If not supported
-    } else alert('Oh... It seems that your browser does not support local storage.\nSticker may not work properly!');
-
-    // If there's not sticker saved in local storage
-    if (!localStorage.sticker) {
-
-        $('.sticker').each(function(index, value) {
-
-            $(value).animate(
-                {'left': Random(0, $('.page').width()-$(value).width()),
-                 'top':  Random(85, $(window).height()-$(value).height())
-                }, 1000, function() { StickerUpdate(value) }
-            );
-        });
-    }
-
-    // Stickers vissiblity init
-    $('.sticker').animate({ 'opacity': 1.0 });
 }
 
-function StickerUpdate(el) {
+function StickerUpdate(el, live) {
 
     var id = StickerId(el);
     var left = $(el).css('left');
@@ -81,6 +111,11 @@ function StickerUpdate(el) {
 function StickerId(el) {
 
     return $(el).attr('data-id');
+}
+
+function StickerEl(id) {
+
+    return $('.sticker[data-id='+id+']');
 }
 
 function StickerSave() {
