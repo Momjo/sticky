447bb51c54ad627789075f1e273108b351103460
Amir Mehdi Savand <amirm3hdi@gmail.com>
2016-03-09 17:21:59 +0400

    Change Sticker functions to a class-like method

--- stickers/static/stickers/js/app.js
+++ stickers/static/stickers/js/app.js
@@ -1,28 +1,86 @@
-// Variables
-
+// Local storage variable
 sticker = {};
 
-// Sticky Functions
+// App class
+Sticker = {
 
-function StickerInit() {
+    // Ajax requests
+    Ajax: {
 
-    // Check for stickers
-    StickerNothing();
+        login: function(event, form, next) {
+            
+            event.preventDefault();
 
-    // jQuery draggable init
-    StickerDraggable();
+            var username  = $(form).children('[name=username]').val();
+            var password  = $(form).children('[name=password]').val();
 
-    // jQuery speed scroll init
-    jQuery.scrollSpeed(500, 500);
+            $.ajax({
+                url: django.url_login,
+                type: 'POST',
+                data: {
+                    'username': username,
+                    'password': password,
+                    'next': next,
+                    'csrfmiddlewaretoken': django.csrf_token
+                },
+                success: function() {
 
-    // If there's any stickers
-    if (StickerCount() > 0) {
+                    location.reload();
+                }
+            });
+        },
 
-        // If local storage is supported
-        if (typeof Storage !== "undefined") {
+        register: function(event, form, next) {
+            
+            event.preventDefault();
 
-            // If sticker is set in local storage
-            if (localStorage.sticker) {
+            var email     = $(form).children('[name=email]').val();
+            var username  = $(form).children('[name=username]').val();
+            var password  = $(form).children('[name=password]').val();
+
+            $.ajax({
+                url: django.url_register,
+                type: 'POST',
+                data: {
+                    'email':    email,
+                    'username': username,
+                    'password': password,
+                    'next': next,
+                    'csrfmiddlewaretoken': django.csrf_token
+                },
+                success: function() {
+
+                    $('form.login').children('[name=username]').val(username);
+                    $('form.login').children('[name=password]').val(password);
+                    $('form.login').submit();
+                }
+            });
+        },
+
+        create: function(event, form) {
+            
+            event.preventDefault();
+
+            var title       = $(form).children('[name=title]').val();
+            var color       = $(form).children('[name=color]').val();
+            var description = $(form).children('[name=description]').val();
+
+            $.ajax({
+                url: django.url_create,
+                type: 'POST',
+                dataType: 'JSON',
+                data: {
+                    'title': title,
+                    'color': color,
+                    'description': description,
+                    'csrfmiddlewaretoken': django.csrf_token
+                },
+                success: function(result) {
+
+                    Sticker.create(result, title, description, color);
+                }
+            });
+        },
 
         delete: function(id) {
             
@@ -43,188 +101,186 @@ function StickerInit() {
         },
     },
 
-                $.each(sticker, function(index, value) {
+    // Functions
+    init: function() {
 
-                    if (parseInt(index) > 0) {
+        // Check for stickers
+        Sticker.nothing();
 
-                        var el = $('.sticker[data-id=' + index + ']');
+        // jQuery draggable init
+        Sticker.draggable();
 
-                        $(el).css('top',  value.top);
-                        $(el).css('left', value.left);
-                    }
+        // jQuery speed scroll init
+        jQuery.scrollSpeed(500, 500);
+
+        // If there's any stickers
+        if (Sticker.count() > 0) {
+
+            // If local storage is supported
+            if (typeof Storage !== "undefined") {
+
+                // If sticker is set in local storage
+                if (localStorage.sticker) {
+
+                    sticker = JSON.parse(localStorage.sticker);
+
+                    $.each(sticker, function(index, value) {
+
+                        if (parseInt(index) > 0) {
+
+                            var el = $('.sticker[data-id=' + index + ']');
+
+                            $(el).css('top',  value.top);
+                            $(el).css('left', value.left);
+                        }
+                    });
+                }
+
+            // If not supported
+            } else alert('Oh... It seems that your browser does not support local storage.\nSticker may not work properly!');
+
+            // If there's not sticker saved in local storage
+            if (!localStorage.sticker) {
+
+                $('.sticker').each(function(index, value) {
+
+                    Sticker.position(value);
                 });
             }
-
-        // If not supported
-        } else alert('Oh... It seems that your browser does not support local storage.\nSticker may not work properly!');
-
-        // If there's not sticker saved in local storage
-        if (!localStorage.sticker) {
-
-            $('.sticker').each(function(index, value) {
-
-                StickerPosition(value);
-            });
         }
-    }
 
-    // Stickers vissiblity init
-    $('.sticker').animate({ 'opacity': 1.0 });
-}
+        // Stickers vissiblity init
+        $('.sticker').animate({ 'opacity': 1.0 });
+    },
 
-function StickerCount() {
-    
-    return $('.sticker-container').children().length;
-}
+    update: function(el, live) {
 
-function StickerNothing() {
-    
-    if (StickerCount() < 1) {
+        var id = Sticker.id(el);
+        var left = $(el).css('left');
+        var top = $(el).css('top');
 
-        $('.oh-no-stickers').fadeIn();
-        $('.sticker-container').hide();
-    
-    } else {
+        sticker[id] = {
+            left: left, 
+            top: top
+        };
 
-        $('.sticker-container').fadeIn();
-        $('.oh-no-stickers').hide();
-    }
-}
+        Sticker.save();
+        Sticker.nothing();
+    },
 
-function StickerDraggable() {
+    save: function() {
 
-    $(".sticker").draggable({
-        handle: ".panel-heading",
-        containment: ".sticker-container",
-        scrollSensitivity: 100,
-        // snap: ".sticker-outer",
-        // snapMode: "outer",
-        stack: ".sticker",
-        start: function() {
-            $(this).addClass('moving');
-        },
-        stop: function(e, ui) {
-            $(this).removeClass('moving');
+        delete sticker["0"];
 
-            StickerUpdate($(this));
+        localStorage.sticker = JSON.stringify(sticker);
+    },
 
-            $('audio:first-of-type')[0].volume = 0.1;
-            $('audio:first-of-type')[0].play();
-        }
-    });
-}
+    create: function(id, title, description, color) {
 
-function StickerUpdate(el, live) {
+        $('.dropdown-create[aria-expanded=true]').dropdown('toggle');
 
-    var id = StickerId(el);
-    var left = $(el).css('left');
-    var top = $(el).css('top');
+        $('.sticker-empty > div > .title').text(title);
+        $('.sticker-empty > div > .description').html(description.replace(/\n/g, "<br>"));
+        $('.sticker-empty > div').removeAttr('no-description');
+        $('.sticker-empty > div').attr('data-id', id);
+        $('.sticker-empty > div').attr('title', 'by You');
+        $('.sticker-empty > div').css('left', 100);
+        $('.sticker-empty > div').css('top', 0);
 
-    sticker[id] = { left: left, top: top };
+        if (description.length < 1)
+            $('.sticker-empty > div').attr('no-description', ' ');
 
-    StickerSave();
-}
+        var new_el = $('.sticker-empty').clone().prependTo('.sticker-container');
+        new_el.removeClass('sticker-empty').children().addClass('sticker');
 
-function StickerId(el) {
+        $('.sticker-empty > div').attr('data-id', 0);
 
-    return $(el).attr('data-id');
-}
+        Sticker.nothing();
+        Sticker.position(new_el.children());
+        Sticker.draggable();
+    },
 
-function StickerEl(id) {
-
-    return $('.sticker[data-id='+id+']');
-}
-
-function StickerSave() {
-
-    localStorage.sticker = JSON.stringify(sticker);
-}
-
-function StickerReset() {
-
-    localStorage.removeItem('sticker');
-    StickerInit();
-    return false;
-}
-
-function StickerCreate(id, title, description, color) {
-
-    $('.dropdown-create[aria-expanded=true]').dropdown('toggle');
-
-    $('.sticker-empty > div > .title').text(title);
-    $('.sticker-empty > div > .description').html(description.replace(/\n/g, "<br>"));
-    $('.sticker-empty > div').removeAttr('no-description');
-    $('.sticker-empty > div').attr('data-id', id);
-    $('.sticker-empty > div').css('left', 100);
-    $('.sticker-empty > div').css('top', 0);
-
-    if (description.length < 1)
-        $('.sticker-empty > div').attr('no-description', ' ');
     delete: function(el) {
 
-    var new_el = $('.sticker-empty').clone().prependTo('.sticker-container');
-    new_el.removeClass('sticker-empty').children().addClass('sticker');
+        $(el).parent().fadeOut(1000, function() {
+            $(this).remove();
+            Sticker.nothing();
+        })
 
-    $('.sticker-empty > div').attr('data-id', 0);
+        delete sticker[Sticker.id(el)];
 
-    StickerNothing();
-    StickerPosition(new_el.children());
-    StickerDraggable();
-}
+        Sticker.save();
+    },
 
-function StickerPosition(el) {
-    
-    $(el).animate(
-        {'left': Random(0, $('.page').width()-$(el).width()),
-         'top':  Random(85, $(window).height()-$(el).height())
-        }, 1000, function() { StickerUpdate(el) }
-    );
-}
+    draggable: function() {
 
-// Base Ajax Functions
+        $(".sticker").draggable({
+            handle: ".panel-heading",
+            containment: ".sticker-container",
+            // scrollSensitivity: 100,
+            // snap: ".sticker-outer",
+            // snapMode: "outer",
+            stack: ".sticker",
+            start: function() {
+                $(this).addClass('moving');
+            },
+            stop: function(e, ui) {
+                $(this).removeClass('moving');
 
-function Login(event, form, token, url, next) {
-    
-    event.preventDefault();
+                Sticker.update($(this));
 
-    var username  = $(form).children('[name=username]').val();
-    var password  = $(form).children('[name=password]').val();
+                $('audio:first-of-type')[0].volume = 0.1;
+                $('audio:first-of-type')[0].play();
+            }
+        });
+    },
 
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {
-            'username': username,
-            'password': password,
-            'next': next,
-            'csrfmiddlewaretoken': token
-        },
-        success: function() {
+    reset: function() {
 
-            location.reload();
+        localStorage.removeItem('sticker');
+        Sticker.init();
+        return false;
+    },
+
+    position: function(el) {
+        
+        $(el).animate(
+            {'left': Random(0, $('.page').width()-$(el).width()),
+             'top':  Random(85, $(window).height()-$(el).height())
+            }, 1000, function() { Sticker.update(el) }
+        );
+    },
+
+    nothing: function() {
+        
+        if (Sticker.count() < 1) {
+
+            $('.oh-no-stickers').fadeIn();
+            $('.sticker-container').hide();
+        
+        } else {
+
+            $('.oh-no-stickers').hide();
+            $('.sticker-container').fadeIn();
         }
-    });
+    },
+
+    id: function(el) {
+
+        return $(el).attr('data-id');
+    },
+
+    el: function(id) {
+
+        return $('.sticker[data-id='+id+']');
+    },
+
+    count: function() {
+        
+        return $('.sticker-container').children().length;
+    },
 }
 
-function Register(event, form, token, url, next) {
-    
-    event.preventDefault();
-
-    var email     = $(form).children('[name=email]').val();
-    var username  = $(form).children('[name=username]').val();
-    var password  = $(form).children('[name=password]').val();
-
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {
-            'email':    email,
-            'username': username,
-            'password': password,
-            'next': next,
-            'csrfmiddlewaretoken': token
-        },
-        success: function() {
 // Init
 
 $(document).ready(Sticker.init());
@@ -242,36 +298,16 @@ $(".sticker-container").on("mouseenter mouseleave dblclick", ".sticker", functio
         }
         case 'mouseleave': {
 
-function Create(event, form, token, url) {
-    
-    event.preventDefault();
-
-    var title       = $(form).children('[name=title]').val();
-    var color       = $(form).children('[name=color]').val();
-    var description = $(form).children('[name=description]').val();
-
-    $.ajax({
-        url: url,
-        type: 'POST',
-        dataType: 'JSON',
-        data: {
-            'title': title,
-            'color': color,
-            'description': description,
-            'csrfmiddlewaretoken': token
-        },
-        success: function(result) {
             $(this).removeClass("hover");
             break;
         }
         case 'dblclick': {
 
-            StickerCreate(result, title, description, color);
             Sticker.Ajax.delete(Sticker.id(this));
             break;
         }
-    });
-}
+    }
+});
 
 // Other
 
