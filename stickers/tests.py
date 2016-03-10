a078623235a64fb1efd0b9b2de8964d9e2f56f24
Amir Mehdi Savand <amirm3hdi@gmail.com>
2016-03-10 11:41:45 +0400

    Update test (Change sticker create status response)

--- stickers/tests.py
+++ stickers/tests.py
@@ -39,7 +39,7 @@ class TestViews(TestCase):
             data=attrs
         )
 
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
         stickers = Sticker.objects.all()
 
